990f8810280e081bf57d7def59ce5d027fd08e5a
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-10-15 15:12:11 +0200

    done (no testing tho.. who needs testing anyways?)

---
+++ main.js
@@ -0,0 +1,15 @@
+import { clients, refreshClients } from "../../private/clients.js";
+
+export class App {
+  constructor(config) {
+    this.config = config;
+  }
+
+  async keys(keys) {
+    await refreshClients();
+    let client = clients[this.config.client];
+    return (await client.request("simulate", "keys", [keys])).result
+      ? "Success"
+      : "Error";
+  }
+}
